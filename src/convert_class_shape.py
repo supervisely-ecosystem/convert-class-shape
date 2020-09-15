3611bfca5c5c0f45a955a3c293c71188577629ca
Maxim Kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-09-15 16:19:13 +0300

    class shape - template (icons and selectors)

--- src/convert_class_shape.py
+++ src/convert_class_shape.py
@@ -27,102 +27,123 @@ def preprocessing(api: sly.Api, task_id, context, state, app_logger):
     sly.logger.info("XXX something here")
 
 
+def shape_to_icon():
+    pass
+
+
 def init_data_and_state(api: sly.Api):
     global ORIGINAL_META
 
     data = {}
     state = {}
+    state["selectors"] = {}
     meta_json = api.project.get_meta(PROJECT_ID)
     ORIGINAL_META = sly.ProjectMeta.from_json(meta_json)
 
+    class_descriptions = {}
     shape_selectors = {}
     for obj_class in ORIGINAL_META.obj_classes:
         obj_class: sly.ObjClass
         possible_shapes = []
         possible_shapes.append({"value": REMAIN_UNCHANGED, "label": REMAIN_UNCHANGED})
 
-        #shape_selectors[obj_class.name] = []
+        transforms = obj_class.geometry_type.allowed_transformations()
+        for gname in transforms:
+            possible_shapes.append({"value": gname, "label": gname})
 
+        shape_selectors[obj_class.name] = possible_shapes
+        state["selectors"][obj_class.name] = REMAIN_UNCHANGED
+
+        class_descriptions[obj_class.name] = {
+            "name": obj_class.name,
+            "color": sly.color.rgb2hex(obj_class.color),
+            "icon": ""
+        }
+
+    data["shapeSelectors"] = shape_selectors
+    return data, state
 
 
 
 def main():
     api = sly.Api.from_env()
 
-    data = {
-        "shapeSelectors": {
-            "person": [
-                {
-                    "value": "remain unchanged",
-                    "label": "remain unchanged"
-                },
-                {
-                    "value": "polygon",
-                    "label": "polygon"
-                },
-                {
-                    "value": "bitmap",
-                    "label": "bitmap"
-                },
-                {
-                    "value": "any",
-                    "label": "any shape"
-                }
-            ],
-            "car": [
-                {
-                    "value": "remain unchanged",
-                    "label": "remain unchanged"
-                },
-                {
-                    "value": "rectangle",
-                    "label": "rectangle"
-                },
-                {
-                    "value": "any",
-                    "label": "any shape"
-                }
-            ],
-            "car1": [
-                {
-                    "value": "remain unchanged",
-                    "label": "remain unchanged"
-                },
-                {
-                    "value": "rectangle",
-                    "label": "rectangle"
-                },
-                {
-                    "value": "any",
-                    "label": "any shape"
-                }
-            ],
-            "car2": [
-                {
-                    "value": "remain unchanged",
-                    "label": "remain unchanged"
-                },
-                {
-                    "value": "rectangle",
-                    "label": "rectangle"
-                },
-                {
-                    "value": "any",
-                    "label": "any shape"
-                }
-            ]
-        }
-    }
+    data, state = init_data_and_state(api)
 
-    state = {
-        "a": "b",
-        "selectors": {
-            "person": "",
-            "car": "",
-            "car1": "",
-            "car2": ""
-        }
-    }
+    # data = {
+    #     "shapeSelectors": {
+    #         "person": [
+    #             {
+    #                 "value": "remain unchanged",
+    #                 "label": "remain unchanged"
+    #             },
+    #             {
+    #                 "value": "polygon",
+    #                 "label": "polygon"
+    #             },
+    #             {
+    #                 "value": "bitmap",
+    #                 "label": "bitmap"
+    #             },
+    #             {
+    #                 "value": "any",
+    #                 "label": "any shape"
+    #             }
+    #         ],
+    #         "car": [
+    #             {
+    #                 "value": "remain unchanged",
+    #                 "label": "remain unchanged"
+    #             },
+    #             {
+    #                 "value": "rectangle",
+    #                 "label": "rectangle"
+    #             },
+    #             {
+    #                 "value": "any",
+    #                 "label": "any shape"
+    #             }
+    #         ],
+    #         "car1": [
+    #             {
+    #                 "value": "remain unchanged",
+    #                 "label": "remain unchanged"
+    #             },
+    #             {
+    #                 "value": "rectangle",
+    #                 "label": "rectangle"
+    #             },
+    #             {
+    #                 "value": "any",
+    #                 "label": "any shape"
+    #             }
+    #         ],
+    #         "car2": [
+    #             {
+    #                 "value": "remain unchanged",
+    #                 "label": "remain unchanged"
+    #             },
+    #             {
+    #                 "value": "rectangle",
+    #                 "label": "rectangle"
+    #             },
+    #             {
+    #                 "value": "any",
+    #                 "label": "any shape"
+    #             }
+    #         ]
+    #     }
+    # }
+    #
+    # state = {
+    #     "a": "b",
+    #     "selectors": {
+    #         "person": "",
+    #         "car": "",
+    #         "car1": "",
+    #         "car2": ""
+    #     }
+    # }
 
     initial_events = [
         {
