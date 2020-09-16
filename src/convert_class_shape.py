6b1f854cdceab5629c4d42201c93f88aba95b7f0
Maxim Kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-09-16 15:14:10 +0300

    clean

--- src/convert_class_shape.py
+++ src/convert_class_shape.py
@@ -1,6 +1,4 @@
 import os
-import random
-import string
 import supervisely_lib as sly
 
 my_app = sly.AppService()
@@ -12,53 +10,16 @@ PROJECT_ID = int(os.environ['modal.state.slyProjectId'])
 ORIGINAL_META = None
 REMAIN_UNCHANGED = "remain unchanged"
 
-# from supervisely_lib.geometry.bitmap import Bitmap
-# from supervisely_lib.geometry.cuboid import Cuboid
-# from supervisely_lib.geometry.point import Point
-# from supervisely_lib.geometry.polygon import Polygon
-# from supervisely_lib.geometry.polyline import Polyline
-# from supervisely_lib.geometry.rectangle import Rectangle
-# from supervisely_lib.geometry.graph import GraphNodes
-# from supervisely_lib.geometry.any_geometry import AnyGeometry
-# from supervisely_lib.geometry.cuboid_3d import Cuboid3d
-# from supervisely_lib.geometry.pointcloud import Pointcloud
-# from supervisely_lib.geometry.point_3d import Point3d
-# from supervisely_lib.geometry.multichannel_bitmap import MultichannelBitmap
-# Bitmap, Cuboid, Point, Polygon, Polyline, Rectangle, GraphNodes, AnyGeometry,
-#                      Cuboid3d, Pointcloud, Point3d, MultichannelBitmap
-
-#@TODO: add other classes
-# zmdi-help
 SHAPE_TO_ICON = {
-    sly.Rectangle: "zmdi zmdi-crop-din",
-    sly.Bitmap: "zmdi zmdi-brush",
-    sly.Polygon: "icons8-polygon",
-    sly.AnyGeometry: "zmdi zmdi-grain",
-    sly.Polyline: "zmdi zmdi-minus",
-    sly.Point: "zmdi zmdi-dot-circle-alt"
+    sly.Rectangle: {"icon": "zmdi zmdi-crop-din", "color": "#ea9d22", "bg": "#fcefd9"},
+    sly.Bitmap: {"icon": "zmdi zmdi-brush", "color": "#ff8461", "bg": "#ffebe3"},
+    sly.Polygon: {"icon": "icons8-polygon", "color": "#2cd26e", "bg": "#d8f8e7"},
+    sly.AnyGeometry: {"icon": "zmdi zmdi-grain", "color": "#e09e11", "bg": "#faf0d8"},
+    sly.Polyline: {"icon": "zmdi zmdi-minus", "color": "#ceadff", "bg": "#f6ebff"},
+    sly.Point: {"icon": "zmdi zmdi-dot-circle-alt", "color": "#899aff", "bg": "#edeeff"},
 }
 
-
-def shape_to_icon(shape):
-    icon = SHAPE_TO_ICON.get(shape)
-    if icon is not None:
-        return '<div class="shape-icon"><i class="{}" style="margin-top: 6px;"></i></div>'.format(icon)
-    else:
-        return '<div>{}</div>'.format("") # graph etc ...
-
-
-@my_app.callback("generate")
-@sly.timeit
-def generate_random_string(api: sly.Api, task_id, context, state, app_logger):
-    rand_string = ''.join((random.choice(string.ascii_letters + string.digits)) for _ in range(LENGTH))
-    rand_string = state["prefix"] + rand_string
-    api.task.set_field(task_id, "data.randomString", rand_string)
-
-
-@my_app.callback("preprocessing")
-@sly.timeit
-def preprocessing(api: sly.Api, task_id, context, state, app_logger):
-    sly.logger.info("XXX something here")
+UNKNOWN_ICON = {"icon": "zmdi zmdi-shape", "color": "#ea9d22", "bg": "#fcefd9"}
 
 
 def init_data_and_state(api: sly.Api):
@@ -78,6 +39,7 @@ def init_data_and_state(api: sly.Api):
             "name": obj_class.name,
             "color": sly.color.rgb2hex(obj_class.color),
             "shape": obj_class.geometry_type.geometry_name(),
+            "shapeIcon": SHAPE_TO_ICON.get(obj_class.geometry_type, UNKNOWN_ICON)
         }
 
         possible_shapes = []
@@ -94,50 +56,12 @@ def init_data_and_state(api: sly.Api):
     return data, state
 
 
-
 def main():
     api = sly.Api.from_env()
-
     data, state = init_data_and_state(api)
 
-    initial_events = [
-        {
-            "state": None,
-            "context": None,
-            "command": "preprocessing",
-        }
-    ]
-
-    # data["table"] = [{
-    #     "date": '2016-05-03',
-    #     "name": 'Tom',
-    #     "address": 'No. 189, Grove St, Los Angeles'
-    # }, {
-    #     "date": '2016-05-02',
-    #     "name": 'Tom',
-    #     "address": 'No. 189, Grove St, Los Angeles'
-    # }, {
-    #     "date": '2016-05-04',
-    #     "name": 'Tom',
-    #     "address": 'No. 189, Grove St, Los Angeles'
-    # }, {
-    #     "date": '2016-05-01',
-    #     "name": 'Tom',
-    #     "address": 'No. 189, Grove St, Los Angeles'
-    # }]
-
-    # data["options"] = [{
-    #     "value": 'Option1',
-    #     "label": 'Option1'
-    # }, {
-    #     "value": 'Option2',
-    #     "label": 'Option2'
-    # }]
-
-    # state["value"] = ""
-
     # Run application service
-    my_app.run(data=data, state=state, initial_events=initial_events)
+    my_app.run(data=data, state=state)
     my_app.wait_all()
 
 
